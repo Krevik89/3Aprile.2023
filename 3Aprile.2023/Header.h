5fccfc9e8774cc0e9476e62c13e28a90ea619229
Govrosha <jimmyblackwood576@outlook.com>
2023-04-14 20:26:42 +0300

    0

--- 3Aprile.2023/Header.h
+++ 3Aprile.2023/Header.h
@@ -172,3 +172,74 @@ class D : public B {
     //catch (...) {
     //    cout << "Eroro";
     //}
+
+//пространство имен
+//using std::cout;
+//namespace name1 {
+//    void Print() {
+//        cout << "Hello\n";
+//    }
+//}
+//namespace {
+//    int a=1;
+//}
+//namespace name2 {
+//    void Print() {
+//        cout << "world\n";
+//    }
+//}
+//namespace name1 {
+//    void Print1() {
+//        cout << "Hello world\n";
+//    }
+//}
+//int a = 8;
+//void f1() {  
+//    int znak;
+//    string str;
+//  
+//    try {
+//         cin >> str;//56+46
+//
+//        if ((znak = str.find_first_of("+-/*",1)) == string::npos)
+//            throw "No znak\n";
+//        int a = stoi(str.substr(0, znak));
+//        int b = stoi(str.substr(znak+1));
+//
+//        switch (str[znak]){
+//        
+//           case '+':
+//               if (a > INT_MAX - b)throw"+";
+//               cout << a + b << endl;
+//               
+//                break;
+//           case '-':
+//               if (a > INT_MIN + b)throw"-";
+//               cout << a - b << endl;
+//                break;
+//           case '*':
+//               if (a < 0 || b < 0) {
+//                   if (a < INT_MIN / b)throw"*";
+//               }
+//               else {
+//                   if (a > INT_MAX / b)throw"*";
+//               }
+//               cout << a * b << endl;
+//                break;
+//           case'/':
+//               if (b != 0)
+//                   cout << a / b << endl;
+//               else throw"/";
+//                break;
+//        default:
+//            break;
+//        }
+//    }
+//     catch (const char* s) {
+//         cout << "Eror " << s;
+//     }
+//     catch (const  out_of_range& e) {
+//         cout << "Eror " << e.what();
+//     }
+//    
+//}
